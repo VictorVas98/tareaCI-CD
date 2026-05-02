1bb572dab200878f81942aca821337739c99b27f
VictorVas98 <abel02172@gmail.com>
2026-05-01 18:29:58 -0600

    implementar cambio

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Bienvenido a mi Despliegue Automático en Azure</h1><p>Versión 1.0</p>"
+    return "<h1>Bienvenido a mi Despliegue Automático en Azure</h1><p>Despliegue creado</p>"
 
 if __name__ == '__main__':
     app.run()
